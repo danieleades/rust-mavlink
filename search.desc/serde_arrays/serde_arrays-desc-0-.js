e4051859d996a0579fd5e967eeb3818e8c0d5749
danieleades <danieleades@users.noreply.github.com>
2025-12-20 14:22:19 +0000

    deploy: bff1f83b004a2f56a3e7df32f613c28c51fb89ac

--- search.desc/serde_arrays/serde_arrays-desc-0-.js
+++
@@ -1,1 +0,0 @@
-searchState.loadedDescShard("serde_arrays", 0, "Serialize and deserialize const generic or …\nDeserialize const generic or arbitrarily-large arrays\nSerialize const generic or arbitrarily-large arrays")
